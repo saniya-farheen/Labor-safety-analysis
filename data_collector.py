f23acdfc5a8633a5d39e0c5e8d2ef2998f285cfc
saniya-farheen <saniyafarheen188@gmail.com>
2024-05-03 21:19:01 +0530

    committed

---
+++ data_collector.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+import time
+import uuid
+
+IMAGE_PATH = "CollectedImage" #path to collect the images
+
+labels = ['Vest', 'Helmet','gloves'] # collecting diffrent images for diffrent folder
+
+number_of_images = 5
+#for collecting images from the folder
+for label in labels:
+    img_path = os.path.join(IMAGE_PATH, label)
+    os.makedirs(img_path)
+
+
+    #open camera
+    cap =cv2.VideoCapture(0)
+    print("collecting images for {label}")
+    time.sleep(5)
+
+    for imgnum in range(number_of_images):
+        ret, frame = cap.read()
+        imagename = os.path.join(IMAGE_PATH, label,label +'.' +' {}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imagename, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(2)
+        #to close the camera condition
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    #release the camera
+    cap.release()
+    cv2.destroyAllWindows()
